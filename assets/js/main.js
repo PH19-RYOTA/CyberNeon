ce9ebf9781ffeb894f1d43391132c5f2e193158c
PH19-RYOTA <52819517+PH19-RYOTA@users.noreply.github.com>
2024-07-19 11:40:02 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// スクロールに応じてヘッダーの色を変更
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 100) {
+      header.classList.add('header-fixed');
+    } else {
+      header.classList.remove('header-fixed');
+    }
+  });
+  
+  // ナビゲーションメニューの開閉
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+  navToggle.addEventListener('click', function() {
+    navMenu.classList.toggle('nav-menu-open');
+  });
+
+  // 横スクロールを実装
+document.addEventListener('DOMContentLoaded', function() {
+    const memberScroll = document.querySelector('.member-scroll');
+
+    memberScroll.addEventListener('wheel', (evt) => {
+        evt.preventDefault();
+        memberScroll.scrollLeft += evt.deltaY;
+    });
+});
